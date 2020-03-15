707e55888ef7efe91977a184ea295e40036822e7
Elias Guere <eliasguere1025@gmail.com>
2020-03-15 12:04:30 -0500

    Creando end-point para registrar usuario de nivel administrador

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,45 @@ const bcrypt = require('bcrypt-nodejs');
 const User = require('../models/User');
 
 function signUp(req, res) {
-  console.log('End Point de signUp');
+  const user = new User();
+
+  const { name, lastname, email, password, repeatPassword } = req.body;
+  user.name = name;
+  user.lastname = lastname;
+  user.email = email;
+  user.role = 'admin';
+  user.actibe = false;
+
+  if (!password || !repeatPassword) {
+    res.status(404).send({ message: 'Las contraseñas son obligatorias' });
+  } else {
+    if (password !== repeatPassword) {
+      res
+        .status(404)
+        .send({ message: 'Las contraseñas tienen que ser iguales' });
+    } else {
+      bcrypt.hash(password, null, null, function(err, hash) {
+        if (err) {
+          res
+            .status(500)
+            .send({ message: 'Error al encriptar la constraseña' });
+        } else {
+          user.password = hash;
+          user.save((err, userStored) => {
+            if (err) {
+              res.status(500).send({ message: 'El usuario ya existe' });
+            } else {
+              if (!userStored) {
+                res.status(404).send({ message: 'Error al crear el usuario' });
+              } else {
+                res.status(200).send({ user: userStored });
+              }
+            }
+          });
+        }
+      });
+    }
+  }
 }
 
 module.exports = {
