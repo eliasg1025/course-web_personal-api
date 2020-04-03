8ee88708c2dad003a37222097f3a73d80bd3b277
Elias Guere <eliasguere1025@gmail.com>
2020-04-02 20:37:49 -0500

    Creando endpoint para obtener usuarios activos e inactivos

--- controllers/user.js
+++ controllers/user.js
@@ -96,8 +96,23 @@ function getUsers(req, res) {
     });
 }
 
+function getUsersActive(req, res) {
+    const query = req.query;
+
+    User.find({ active: query.active }).then(users => {
+        if (!users) {
+            res.status(404).send({
+                message: 'No se ha encontrado ningun usuario'
+            });
+        } else {
+            res.status(200).send({ users });
+        }
+    });
+}
+
 module.exports = {
     signUp,
     signIn,
-    getUsers
+    getUsers,
+    getUsersActive
 };
