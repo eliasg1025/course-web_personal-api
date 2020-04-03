ca241158bcd7c5fc8a11503408aa9d8de992db24
Elias Guere <eliasguere1025@gmail.com>
2020-04-02 19:13:16 -0500

    Creando endpoint para obtener todos los usuarios

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const app = express();
 const { API_VERSION } = require('./config');
@@ -10,17 +11,21 @@ const userRoutes = require('./routes/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 // Configure Header HTTP
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
+    );
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
 });
 
 // Basic routes
--- controllers/user.js
+++ controllers/user.js
@@ -3,81 +3,101 @@ const jwt = require('../services/jwt');
 const User = require('../models/User');
 
 function signUp(req, res) {
-  const user = new User();
+    const user = new User();
 
-  const { email, password, repeatPassword } = req.body;
-  user.email = email.toLowerCase();
-  user.role = 'admin';
-  user.active = false;
+    const { email, password, repeatPassword } = req.body;
+    user.email = email.toLowerCase();
+    user.role = 'admin';
+    user.active = false;
 
-  if (!password || !repeatPassword) {
-    res.status(404).send({ message: 'Las contraseñas son obligatorias' });
-  } else {
-    if (password !== repeatPassword) {
-      res
-        .status(404)
-        .send({ message: 'Las contraseñas tienen que ser iguales' });
+    if (!password || !repeatPassword) {
+        res.status(404).send({ message: 'Las contraseñas son obligatorias' });
     } else {
-      bcrypt.hash(password, null, null, function(err, hash) {
-        if (err) {
-          res
-            .status(500)
-            .send({ message: 'Error al encriptar la constraseña' });
+        if (password !== repeatPassword) {
+            res.status(404).send({
+                message: 'Las contraseñas tienen que ser iguales'
+            });
         } else {
-          user.password = hash;
-          user.save((err, userStored) => {
-            if (err) {
-              res.status(500).send({ message: 'El usuario ya existe' });
-            } else {
-              if (!userStored) {
-                res.status(404).send({ message: 'Error al crear el usuario' });
-              } else {
-                res.status(200).send({ user: userStored });
-              }
-            }
-          });
+            bcrypt.hash(password, null, null, function(err, hash) {
+                if (err) {
+                    res.status(500).send({
+                        message: 'Error al encriptar la constraseña'
+                    });
+                } else {
+                    user.password = hash;
+                    user.save((err, userStored) => {
+                        if (err) {
+                            res.status(500).send({
+                                message: 'El usuario ya existe'
+                            });
+                        } else {
+                            if (!userStored) {
+                                res.status(404).send({
+                                    message: 'Error al crear el usuario'
+                                });
+                            } else {
+                                res.status(200).send({ user: userStored });
+                            }
+                        }
+                    });
+                }
+            });
         }
-      });
     }
-  }
 }
 
 function signIn(req, res) {
-  const params = req.body;
-  const email = params.email.toLowerCase();
-  const password = params.password;
+    const params = req.body;
+    const email = params.email.toLowerCase();
+    const password = params.password;
 
-  User.findOne({ email }, (err, userStored) => {
-    if (err) {
-      res.status(500).send({ message: 'Error del servidor' });
-    } else {
-      if (!userStored) {
-        res.status(404).send({ message: 'Usuario no encontrado' });
-      } else {
-        bcrypt.compare(password, userStored.password, (err, check) => {
-          if (err) {
+    User.findOne({ email }, (err, userStored) => {
+        if (err) {
             res.status(500).send({ message: 'Error del servidor' });
-          } else if (!check) {
-            res.status(404).send({ message: 'La contraseña es incorrecta' });
-          } else {
-            if (!userStored.active) {
-              res
-                .status(200)
-                .send({ code: 200, message: 'El usuario no se ha activado' });
+        } else {
+            if (!userStored) {
+                res.status(404).send({ message: 'Usuario no encontrado' });
             } else {
-              res.status(200).send({
-                accessToken: jwt.createAccessToken(userStored),
-                refreshToken: jwt.createRefreshToken(userStored)
-              });
+                bcrypt.compare(password, userStored.password, (err, check) => {
+                    if (err) {
+                        res.status(500).send({ message: 'Error del servidor' });
+                    } else if (!check) {
+                        res.status(404).send({
+                            message: 'La contraseña es incorrecta'
+                        });
+                    } else {
+                        if (!userStored.active) {
+                            res.status(200).send({
+                                code: 200,
+                                message: 'El usuario no se ha activado'
+                            });
+                        } else {
+                            res.status(200).send({
+                                accessToken: jwt.createAccessToken(userStored),
+                                refreshToken: jwt.createRefreshToken(userStored)
+                            });
+                        }
+                    }
+                });
             }
-          }
-        });
-      }
-    }
-  });
+        }
+    });
+}
+
+function getUsers(req, res) {
+    User.find().then(users => {
+        if (!users) {
+            res.status(404).send({
+                message: 'No se ha encontrado ningun usuario'
+            });
+        } else {
+            res.status(200).send({ users });
+        }
+    });
 }
 
 module.exports = {
-  signUp,
-  signIn
+    signUp,
+    signIn,
+    getUsers
 };
