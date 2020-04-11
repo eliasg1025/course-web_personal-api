ca60f1fe3bcf40b6761a509694ee3588eca7ff77
Elias Guere <eliasguere1025@gmail.com>
2020-04-11 16:01:04 -0500

    Creando estructura y modelo de Menu en el server

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { API_VERSION } = require('./config');
 // Load routings
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
+const menuRoutes = require('./routes/menu');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -31,5 +32,6 @@ app.use((req, res, next) => {
 // Basic routes
 app.use(`/api/${API_VERSION}`, authRoutes);
 app.use(`/api/${API_VERSION}`, userRoutes);
+app.use(`/api/${API_VERSION}`, menuRoutes);
 
 module.exports = app;
---
+++ controllers/menu.js
@@ -0,0 +1,9 @@
+const Menu = require('../models/Menu');
+
+function addMenu(req, res) {
+    console.log('Add Menu ...');
+}
+
+module.exports = {
+    addMenu
+};
