53e0810097a1112480c0f7c01d5eed35ced2e92e
Elias Guere <eliasguere1025@gmail.com>
2020-04-10 23:14:36 -0500

    Creando end point para eliminar usuarios

--- controllers/user.js
+++ controllers/user.js
@@ -246,6 +246,24 @@ function activateUser(req, res) {
     });
 }
 
+function deleteUser(req, res) {
+    const { id } = req.params;
+
+    User.findByIdAndUpdate(id, (err, userDeleted) => {
+        if (err) {
+            res.status(500).send({ message: 'Error del servidor' });
+        } else {
+            if (!userDeleted) {
+                res.status(404).send({ message: 'Usuario no encontrado' });
+            } else {
+                res.status(200).send({
+                    message: 'El usuario ha sido eliminado correctamente'
+                });
+            }
+        }
+    });
+}
+
 module.exports = {
     signUp,
     signIn,
@@ -254,5 +272,6 @@ module.exports = {
     uploadAvatar,
     getAvatar,
     updateUser,
-    activateUser
+    activateUser,
+    deleteUser
 };
