613ae42f7304cc8538bba1983e523d688c1971da
Elias Guere <eliasguere1025@gmail.com>
2020-04-11 19:54:30 -0500

    Creando end points para crear y listar los menus

--- controllers/menu.js
+++ controllers/menu.js
@@ -1,9 +1,43 @@
 const Menu = require('../models/Menu');
 
 function addMenu(req, res) {
-    console.log('Add Menu ...');
+    const { title, url, order, active } = req.body;
+    const menu = new Menu();
+
+    menu.title = title;
+    menu.url = url;
+    menu.order = order;
+    menu.active = active;
+    menu.save((err, createdMenu) => {
+        if (err) {
+            res.status(500).send({ message: 'Error del servidor' });
+        } else {
+            if (!createdMenu) {
+                res.status(404).send({ message: 'Error al crear el menu' });
+            } else {
+                res.status(200).send({ message: 'Menu creado correctamente' });
+            }
+        }
+    });
+}
+
+function getMenus(req, res) {
+    Menu.find()
+        .sort({ order: 'asc' })
+        .exec((err, menusStored) => {
+            if (err) {
+                res.status(500).send({ message: 'Error del servidr' });
+            } else {
+                if (!menusStored || menusStored.length === 0) {
+                    res.status(404).send({ message: 'No se ha encontrado ningun menu' });
+                } else {
+                    res.status(200).send({ menus: menusStored });
+                }
+            }
+        })
 }
 
 module.exports = {
-    addMenu
+    addMenu,
+    getMenus
 };
