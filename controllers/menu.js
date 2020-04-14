6999a79c66ca550ac782cac3354c2ad474b1252b
Elias Guere <eliasguere1025@gmail.com>
2020-04-13 23:57:19 -0500

    Creando end point para la eliminacion de menu

--- controllers/menu.js
+++ controllers/menu.js
@@ -87,9 +87,28 @@ function activateMenu(req, res) {
     });
 }
 
+function deleteMenu(req, res) {
+    const { id } = req.params;
+
+    Menu.findByIdAndRemove(id, (err, menuDeleted) => {
+        if (err) {
+            res.status(500).send({ message: 'Error del servidor' });
+        } else {
+            if (!menuDeleted) {
+                res.status(404).send({ message: 'Menu no encontrado' });
+            } else {
+                res.status(200).send({
+                    message: 'El Menu ha sido eliminado correctamente'
+                });
+            }
+        }
+    });
+}
+
 module.exports = {
     addMenu,
     getMenus,
     updateMenu,
-    activateMenu
+    activateMenu,
+    deleteMenu
 };
