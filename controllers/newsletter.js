cbef7ba2eb984ea1c560255d0ba202cd72301d71
Elias Guere <eliasguere1025@gmail.com>
2020-04-19 19:44:07 -0500

    Creando end point para guardar los emails de la newsletter

--- controllers/newsletter.js
+++ controllers/newsletter.js
@@ -1,7 +1,25 @@
 const Newsletter = require('../models/Newsletter.js');
 
 function suscribeEmail(req, res) {
-	console.log('Suscribe email ok');
+	const email = req.params.email;
+	const newsletter = new Newsletter();
+
+	if (!email) {
+		res.status(404).send({ code: 404, message: 'El email es obligatorio' });
+	} else {
+		newsletter.email = email.toLowerCase();
+		newsletter.save((err, newsletterStored) => {
+			if (err) {
+				res.status(500).send({ code: 500, message: 'El email ya existe' });
+			} else {
+				if (!newsletterStored) {
+					res.status(400).send({ code: 400, message: 'Error al registrar en la newsletter' });
+				} else {
+					res.status(200).send({ code: 200, message: 'Email registrado correctamente' });
+				}
+			}
+		});
+	}
 }
 
 module.exports = {
