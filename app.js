97b674830871145edc04602c234007b3910bb0eb
Elias Guere <eliasguere1025@gmail.com>
2020-04-19 19:36:38 -0500

    Creando modelo y controlador de newsletters

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const { API_VERSION } = require('./config');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const menuRoutes = require('./routes/menu');
+const newsletterRoutes = require('./routes/newsletter.js');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -33,5 +34,6 @@ app.use((req, res, next) => {
 app.use(`/api/${API_VERSION}`, authRoutes);
 app.use(`/api/${API_VERSION}`, userRoutes);
 app.use(`/api/${API_VERSION}`, menuRoutes);
+app.use(`/api/${API_VERSION}`, newsletterRoutes);
 
 module.exports = app;
---
+++ controllers/newsletter.js
@@ -0,0 +1,9 @@
+const Newsletter = require('../models/Newsletter.js');
+
+function suscribeEmail(req, res) {
+	console.log('Suscribe email ok');
+}
+
+module.exports = {
+	suscribeEmail
+}
